dc0f85aedf3776c556386edd9d95f941e3356b18
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-03-26 15:05:16 -0300

    linked list started

---
+++ deque/deque.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    int val;
+    struct node* next;
+    struct node* prev;
+} node;
+
+typedef struct list{
+    size_t size;
+    node* head;
+    node* tail;
+} list;
+
+
+list* create_list(){
+
+}
+
+node* create_node(){
+
+}
+
+void append(){
+
+}
+
+int pop(){
+
+}
+
+int remove(){
+
+}
+
+size_t search(){
+
+}
+
+size_t len(){
+
+}
+
+int main(){
+
+    return 0;
+}

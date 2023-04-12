7b54afd287ce67061a1406b15eefee12cf496256
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-04-12 15:10:38 -0300

    debugged circular, implemented insert function

--- datastructures/linklist/circular.c
+++ datastructures/linklist/circular.c
@@ -48,30 +48,50 @@ int append(clist** list, int key){
     return 1;
 }
 
-int insert(){
+int __reverse_apend(clist** list, int key){
+    node* new = __create_node(key);
+    new -> next = (*list) -> head;
+    (*list) -> pseudotail -> next = new;
+    (*list) -> head = new;
+    return 1;
+}
+
+int insert(clist** list, int key, size_t pos){
+    node* new = __create_node(key);
+    if((*list) -> size < 2 || pos % (*list) -> size == 0) return append(list, key);
+    if(pos == 0) return __reverse_apend(list,key);
+    node* aux = (*list)->head;
+    for(size_t i = 0; i < pos-1; i++) aux = aux -> next;
+    node* aux2 = aux -> next;
+    aux -> next = new;
+    new -> next = aux2;
+    return 1;
+}
+
+int pop(clist** list){
 
 }
 
-int pop(){
+int __pop_first(clist** list){
 
 }
 
-int remove(){
+int remove_from_list(clist** list, size_t pos){
 
 }
 
 void print_circular_list(clist* list){
     node* aux = list -> head;
     if(list->size == 1){
-        // TODO
+        printf("%d\n", aux -> value);
         return;
     }
     while(aux != list -> pseudotail){
         printf("%d ", aux -> value);
         aux = aux -> next;
     }
-    aux = aux -> next;
     printf("%d\n", aux->value);
+    aux = aux -> next;
 }
 
 size_t len(clist* list){
@@ -90,4 +110,16 @@ void free_all(clist** list){
     // }
     // aux = aux -> next;
     // printf("%d\n", aux->value);
+}
+
+int main(){
+    clist* l = create_circular_list();
+    append(&l, 1);
+    append(&l, 2);
+    append(&l, 3);
+    printf("%d\n", l->size);
+    // printf("%d", l->pseudotail->value);
+    insert(&l,4,2);
+    print_circular_list(l);
+    return 0;
 }

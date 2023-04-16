823b68419f2737780859ac917b9fe6452796da17
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-04-16 19:51:58 -0300

    brainstorming dataframe

--- stl+/dataframe.cpp
+++ stl+/dataframe.cpp
@@ -7,8 +7,49 @@ using namespace std;
 // a special vector-like container (Series) and the rhs is any vector-like container 
 
 
+template <typename T>
 class Series{
+    private:
+        vector<T> data;
 
+    public:
+
+        Series() {}
+
+        Series(vector<T> x){
+            data = x;
+        }
+        vector<T> get() const{
+            return data;
+        }
+
+        void show() const{
+            for(auto x : data){
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+
+        void add(T x){
+            data.push_back(x);
+        }
+
+        void remove(T x){
+            data.erase(find(data.begin(), data.end(), x));
+        }
+
+        T& operator[](size_t i){
+            return data[i];
+        }
+
+        Series<T>& operator+(T x) const{
+            Series<T> y(*this);
+            for(auto element : y.data){
+                element += x;
+            }
+            return y;
+        }
+        // TODO: add operator+, operator-,operator*,operator/ as a operate-in-all-elements ,operator=, ...
 };
 
 
--- stl+/range.cpp
+++ stl+/range.cpp
@@ -62,43 +62,81 @@ class Range{
 
         class Iterator{
             public:
-                Iterator(T curr, T step) : curr{curr}, step{step} {}
+                Iterator(T curr, T step, size_t maximum) : curr{curr}, step{step}, maximum{maximum} {}
 
-                Iterator& operator++(){
-                    curr += step;
-                    return *this;
+                // TODO: operator+, operator-, operator=, operator-- (both), operator bool, ...
+
+                bool operator==(const Iterator& other) const{
+                    return curr==other.curr && step==other.step;
                 }
 
-                // Iterator operator++(int){
-                //     curr += step;
-                //     return *this;
-                // }
+                bool operator!=(const Iterator& other) const{
+                    return curr!=other.curr || step!=other.step;
+                }
+
+                Iterator& operator+(const T& passages){
+                    if(counter + passages <= maximum){
+                        curr += step*passages;
+                        counter += passages;
+                        return *this;
+                    } else throw out_of_range("iterator overflow");   
+                }
+
+                Iterator& operator++(){
+                    if(counter+1<= maximum){
+                        curr += step;
+                        return *this;
+                    } else throw out_of_range("iterator overflow");
+                }
+
+                Iterator operator++(int){
+                    if(counter + 1 <= maximum){
+                        Iterator current(curr,step);
+                        curr += step;
+                        counter++;
+                        return current;
+                    } else throw out_of_range("iterator overflow");
+                }
+
+                Iterator& operator-(const T& passages){
+                    if(counter - passages >= 0){
+                        curr -= step*passages;
+                        counter -= passages;
+                        return *this;
+                    } else throw out_of_range("iterator underflow");
+                }
+
+                Iterator& operator--(){
+                    if(counter - 1 >= 0){
+                        curr -= step;
+                        counter -= 1;
+                        return *this;
+                    } else throw out_of_range("iterator underflow");
+                }
+
+                Iterator operator--(int){
+                }
 
                 T operator*() const{
                     return curr;
                 }
 
-                bool operator==(const Iterator& other) const{
-                    return curr == other.curr;
-                }
-
-                bool operator!=(const Iterator& other) const{
-                    return curr!= other.curr;
-                }
             private:
                 T curr;
                 T step;
+                size_t counter;
+                size_t maximum;
 
         };
 
         Iterator begin() const{
-            return Iterator{initial_value, step};
+            return Iterator{initial_value, step, amount()};
         }
 
         Iterator end() const{
             size_t temp = initial_value;
             temp = temp + step*amount();
-            return Iterator{temp,step};
+            return Iterator{temp, step, amount()};
         }
 
         // iterator vec_begin(){
@@ -118,9 +156,11 @@ class Range{
 };
 
 class range: public Range<size_t>{
-    // TODO: constructors and operator=
     using Range::Range;
 };
+
+// reversed(range)
+
 
 // Proof of concept: slicing
 template<typename T>
